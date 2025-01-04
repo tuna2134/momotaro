f2e769141e1e4f8ff825caf2a8583016df3442e1
コマリン親衛隊 <hello@tuna2134.dev>
2025-01-04 19:28:27 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <atomic>
+#include <csignal>
+
+std::atomic<bool> running(true);
+
+void handleSignal(int signum) {
+    if (signum == SIGINT) {
+        std::cout << "OGYAA.." << std::endl;
+        running = false;
+    }
+}
 
 int main() {
-    while (true) {
+    signal(SIGINT, signalHandler);
+    
+    while (running) {
         std::cout << "OGYAA!" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
