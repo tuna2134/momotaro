bdb5b30ce7ee5b9b7bb49274cad991078fa6e238
tuna2134 <hello@tuna2134.dev>
2025-01-04 03:51:22 +0000

    sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main() {
     while (true) {
         std::cout << "OGYAA!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     return 0;
